596f145ca50ee0e176969726ede8aaa8ad07db1f
jeongmin-ahn122 <jeongmin.ahn.122@gmail.com>
2019-11-20 00:36:45 -0800

    Fixed the layout bug

--- app/src/main/java/com/example/myapplication/NewAgencyFragment.java
+++ app/src/main/java/com/example/myapplication/NewAgencyFragment.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -34,6 +37,9 @@ public class NewAgencyFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.new_agency_fragment, container, false);
 
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getDialog().setCanceledOnTouchOutside(false);
+
         listener = (NameAgencyDialogListener) getActivity();
 
         nameBox = (EditText)view.findViewById(R.id.NameAgencyBox);
@@ -42,11 +48,33 @@ public class NewAgencyFragment extends DialogFragment {
         acceptButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                listener.onFinishNameDialog(nameBox.getText().toString());
-                dismiss();
+                if(nameBox.getText().toString().equals(""))
+                {
+                    Toast toast = Toast.makeText(getContext(), "Please enter a name", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else
+                {
+                    listener.onFinishNameDialog(nameBox.getText().toString());
+                    dismiss();
+                }
             }
         });
 
         return view;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Window window = getDialog().getWindow();
+
+        final float scale = getResources().getDisplayMetrics().density;
+        int width = (int) (350 * scale);
+        int height = (int) (300 * scale);
+
+        window.setLayout(width, height);
+        window.setGravity(Gravity.CENTER);
     }
 }
