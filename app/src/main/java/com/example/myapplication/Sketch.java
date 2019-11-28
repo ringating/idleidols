832df24be8ea8071140496c5140738f4e0b5967e
ZigZiggyZag <nicka.j384@gmail.com>
2019-11-28 02:58:47 -0800

    Idol has been trapped in the PApplet, Pressing Idol still increases currency

--- app/src/main/java/com/example/myapplication/Sketch.java
+++ app/src/main/java/com/example/myapplication/Sketch.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 import processing.core.PApplet;
 import processing.core.PGraphics;
@@ -27,6 +29,7 @@ public class Sketch extends PApplet {
 
         void scroll()
         {
+            imageMode(CORNER);
             image(image, -(x%displayWidth), -(y%displayHeight));
             image(image, -((x%displayWidth) - displayWidth), -(y%displayHeight));
             image(image, -(x%displayWidth), -((y%displayHeight) - displayHeight));
@@ -49,18 +52,141 @@ public class Sketch extends PApplet {
         }
     }
 
+    class DisplayIdol {
+
+        private int x;
+        private int y;
+
+        private PImage body;
+        private PImage head;
+
+        int xHeadOffset;
+        int yHeadOffset;
+
+        int yBodyOffset;
+
+        int xLeftBound;
+        int xRightBound;
+        int yTopBound;
+        int yBottomBound;
+
+        int xBoundOffset;
+        int yBoundOffset;
+
+        DisplayIdol(int x, int y, int xHeadOffset, int yHeadOffset, int yBodyOffset, int xBoundOffset, int yBoundOffset, double sizeMultiplier, String bodyImage, String headImage)
+        {
+            this.x = x;
+            this.y = y;
+
+            this.body = loadImage(bodyImage);
+            this.head = loadImage(headImage);
+
+            body.resize((int)(body.width * sizeMultiplier), (int)(body.height * sizeMultiplier));
+            head.resize((int)(900 * sizeMultiplier),(int)(900 * sizeMultiplier));
+
+            this.xHeadOffset = (int)(xHeadOffset * sizeMultiplier);
+            this.yHeadOffset = (int)(yHeadOffset * sizeMultiplier);
+
+            this.yBodyOffset = (int)(yBodyOffset * sizeMultiplier);
+
+            this.xBoundOffset = (int)(xBoundOffset * sizeMultiplier);
+            this.yBoundOffset = (int)(yBoundOffset * sizeMultiplier);
+
+            generateBounds();
+        }
+
+        @SuppressWarnings("SameParameterValue")
+        void draw()
+        {
+            imageMode(CENTER);
+            image(body, x, y + this.yBodyOffset);
+            image(head, x + this.xHeadOffset, y + this.yHeadOffset);
+        }
+
+        void changeHead(String headImage, int xHeadOffset, int yHeadOffset)
+        {
+            this.head = loadImage(headImage);
+            this.xHeadOffset = xHeadOffset;
+            this.yHeadOffset = yHeadOffset;
+            generateBounds();
+        }
+
+        void generateBounds()
+        {
+            this.xLeftBound = (x - body.width/2) - xBoundOffset;
+            this.xRightBound = (x + body.width/2) + xBoundOffset;
+            this.yBottomBound = ((y + body.height/2) + yBodyOffset) + yBoundOffset;
+            this.yTopBound = (((y - body.height/2) + yBodyOffset) -  (head.height - (((y + head.height/2) + yHeadOffset) - ((y - body.height/2) + yBodyOffset)))) - yBoundOffset;
+        }
+
+        boolean isMouseInIdol()
+        {
+            boolean isInIdol = false;
+
+            if ((mouseX > xLeftBound && mouseX < xRightBound) && (mouseY < yBottomBound && mouseY > yTopBound))
+            {
+                isInIdol = true;
+            }
+
+            return isInIdol;
+        }
+    }
+
     public void settings() {
         size(displayWidth, displayHeight, OPENGL);
     }
 
     private ScrollingBackground homeBackground;
+    private DisplayIdol idol;
+    private int idolX;
+    private int idolY;
+    private boolean incrementMonies;
+    private boolean drawEffect;
+
+    Agency agency;
+    TextView currency;
+    Activity act;
 
     public void setup() {
+
+        act = this.getActivity();
+        agency = (Agency) act.getApplicationContext();
+        currency = act.findViewById(R.id.currency);
+
         frameRate(60);
         homeBackground = new ScrollingBackground(0, 0, 1, 1, "background.png");
+        idolX = displayWidth/2;
+        idolY = (displayHeight/2) - 150;
+        idol = new DisplayIdol(idolX, idolY, -40, -300, 400, -200, -150, 0.8,"body.png", "onionHead.png");
     }
 
     public void draw() {
         homeBackground.scroll();
+        idol.draw();
+        textSize(40);
+        fill(0,0,0);
+        if (idol.isMouseInIdol())
+        {
+            text("PRESSED", 0, displayHeight/2);
+        }
+        else
+        {
+            text("NOT PRESSED", 0, displayHeight/2);
+        }
+    }
+
+
+    public void mousePressed()
+    {
+        if (idol.isMouseInIdol())
+        {
+            agency.SetCurrency(agency.GetCurrentCurrency() + 10);
+            act.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    currency.setText(Integer.toString(agency.GetCurrentCurrency()));
+                }
+            });
+        }
     }
 }
